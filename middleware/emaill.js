f57b9763018b6e3dc5028178712f6d03c2b743b3
Devanshu7828 <devanshurajak23@gmail.com>
2021-04-05 09:54:52 +0530

    remove env

--- middleware/emaill.js
+++
@@ -1,6 +0,0 @@
-const jwt = require("jsonwebtoken");
-const mailgun = require("mailgun-js");
-
-const mg = mailgun({ apiKey: process.env.MAILGUN_APIKEY, domain: process.env.DOMAIN })
-
-module.exports = mg;
